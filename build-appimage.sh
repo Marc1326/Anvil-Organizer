a4141f962e0b324ad7ecf35acdb16b80eea67891
Marc <github.importer709@passmail.com>
2026-04-09 11:40:03 +0200

    v1.4.7

--- build-appimage.sh
+++ build-appimage.sh
@@ -14,7 +14,7 @@ APPDIR="$SCRIPT_DIR/AnvilOrganizer.AppDir"
 RELEASE_DIR="$SCRIPT_DIR/release"
 APPIMAGETOOL="/tmp/appimagetool"
 
-VERSION="1.4.6"
+VERSION="1.4.7"
 APPIMAGE_NAME="Anvil_Organizer-${VERSION}-x86_64.AppImage"
 
 echo "========================================"
--- packaging/rpm/anvil-organizer.spec
+++ packaging/rpm/anvil-organizer.spec
@@ -1,5 +1,5 @@
 Name:           anvil-organizer
-Version:        1.4.6
+Version:        1.4.7
 Release:        1%{?dist}
 Summary:        Native Linux Mod Manager inspired by Mod Organizer 2
 
